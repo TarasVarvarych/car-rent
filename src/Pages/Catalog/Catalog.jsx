700003a41a84cd8a4f43db4518fabcafdc7087d5
TarasVarvarych <varvarychtaras@gmail.com>
2023-09-18 14:19:28 +0300

    Update Catalog.jsx

--- src/Pages/Catalog/Catalog.jsx
+++ src/Pages/Catalog/Catalog.jsx
@@ -2,19 +2,27 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { CarList } from "../../components/CarList/CarList";
 import { LoadMoreButton } from "../../components/LoadMoreButton/LoadMoreButton";
+import { Loader } from "../../components/Loader/Loader";
 
 const URL = "https://65056b0eef808d3c66f00342.mockapi.io/adverts";
 
 const Catalog = () => {
   const [cars, setCars] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [displayedCars, setDisplayedCars] = useState([]);
   const initialDisplayCount = 8;
 
   useEffect(() => {
-    axios(URL).then((ads) => {
-      setCars(ads.data);
-      setDisplayedCars(ads.data.slice(0, initialDisplayCount));
-    });
+    axios(URL)
+      .then((ads) => {
+        setCars(ads.data);
+        setDisplayedCars(ads.data.slice(0, initialDisplayCount));
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
   }, []);
 
   const loadMoreCars = () => {
@@ -27,9 +35,15 @@ const Catalog = () => {
   }, []);
   return (
     <section>
-      <CarList cars={displayedCars} />
-      {displayedCars.length < cars.length && (
-        <LoadMoreButton onClick={loadMoreCars} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <CarList cars={displayedCars} />
+          {displayedCars.length < cars.length && (
+            <LoadMoreButton onClick={loadMoreCars} />
+          )}
+        </>
       )}
     </section>
   );
